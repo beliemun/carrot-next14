7e75b3f736a05de379851480edbb517d2490d196
beliemun <beliemun@paran.com>
2024-06-11 14:22:12 +0900

    #8.13 Recap

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+export default () => {
+  return (
+    <main className="h-screen w-full">
+      <div className="p-4">
+        <h1 className="text-3xl">상품</h1>
+        <p className="mt-2">자세한 설명</p>
+      </div>
+    </main>
+  );
+};
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,31 @@
 import { cookies } from "next/headers";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
+
+type Routes = {
+  [key: string]: boolean;
+};
+
+const pulbicOnlyUrls: Routes = {
+  "/": true,
+  "/sign-in": true,
+  "/sign-in-by-sms": true,
+  "/sign-up": true,
+};
 
 export const middleware = async (request: NextRequest) => {
   const session = await getSession();
-  console.log(session);
-  if (request.nextUrl.pathname === "/profile") {
-    return Response.redirect(new URL("/", request.url));
+  const exists = pulbicOnlyUrls[request.nextUrl.pathname];
+  if (!session.id) {
+    // session.id가 없다는 것은 로그아웃이 되었다는 것을 뜻함
+    if (!exists) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    // 로그인 된 상태의 처리
+    if (exists) {
+      return NextResponse.redirect(new URL("/products", request.url));
+    }
   }
 };
 
