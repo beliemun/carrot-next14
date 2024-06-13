29cfa7d717ea9c1c874903fbf35e9019e9d2f8f8
beliemun <beliemun@paran.com>
2024-06-13 19:00:16 +0900

    Update route.ts

--- src/app/sign-in/github/route.ts
+++ src/app/sign-in/github/route.ts
@@ -5,6 +5,7 @@ export const GET = () => {
     scope: "read:user,read:email",
     allow_signup: "true",
   };
+
   // URLSearchParams는 object를 URL 뒤에서 이어지는 쿼리 파라미터 형식으로 변환해준다.
   const formattedParams = new URLSearchParams(params).toString();
   const url = `${BASE_URL}?${formattedParams}`;
